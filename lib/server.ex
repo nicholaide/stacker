08786464d3cc84fa649a14f12b2b88ee75f93165
Nicholai de Guzman <Nicholai@cchinfatt.local>
2016-04-23 18:31:38 -0500

    implement the API

--- lib/server.ex
+++ lib/server.ex
@@ -1,5 +1,23 @@
 defmodule Stacker.Server do
   use GenServer # this handles all the callbacks
+  
+  ####
+  # External API
+  
+  def start_link( [head | tail] ) do
+    GenServer.start_link(__MODULE__, [head | tail], name: __MODULE__)
+  end
+  
+  def pop do
+    GenServer.call __MODULE__, :pop    
+  end
+  
+  def push(new_item) do
+    GenServer.cast __MODULE__, {:push, new_item}
+  end
+  
+  ####
+  # GenServer implementation  
     
   def handle_call(:pop, _from, [head | tail]) do
     { :reply, head, tail }
@@ -9,8 +27,8 @@ defmodule Stacker.Server do
     { :reply, nil, [] }
   end
   
-  def handle_cast({:push, new_number}, [head | tail]) do
-    { :noreply, [new_number] ++ [head | tail] }
+  def handle_cast({:push, new_item}, [head | tail]) do
+    { :noreply, [new_item] ++ [head | tail] }
   end
     
 end

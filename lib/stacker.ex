dd1130eade2cbe886f45ad9984067edfaaa2646c
Nicholai de Guzman <Nicholai@cchinfatt.local>
2016-04-24 14:29:28 -0500

    seems to fail
    - server.ex isn't in the subfolder; idx -S mix command fails

--- lib/stacker.ex
+++ lib/stacker.ex
@@ -1,2 +1,16 @@
 defmodule Stacker do
+  use Application
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      worker(Stacker.Server, ["little", "stuart", 90])      
+    ]
+
+    opts = [strategy: :one_for_one, name: Stacker.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
 end
